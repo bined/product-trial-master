a4527e41e12bf68d92d26416e7e52a7142d36f13
bined <binedsoulaiman@gmail.com>
2024-12-05 17:40:57 +0100

    Protect product routes

---
+++ back/src/EventSubscriber/ProductSecuritySubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ProductSecuritySubscriber implements EventSubscriberInterface
+{
+    private const ADMIN_EMAIL = 'admin@admin.com';
+    private const PROTECTED_METHODS = ['POST', 'PUT', 'PATCH', 'DELETE'];
+
+    public function __construct(
+        private Security $security
+    ) {}
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::CONTROLLER => 'onKernelController',
+        ];
+    }
+
+    public function onKernelController(ControllerEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        if (!str_starts_with($request->getPathInfo(), '/api/products')) {
+            return;
+        }
+
+        if (!in_array($request->getMethod(), self::PROTECTED_METHODS)) {
+            return;
+        }
+
+        $user = $this->security->getUser();
+
+        if (!$user || $user->getUserIdentifier() !== self::ADMIN_EMAIL) {
+            throw new AccessDeniedHttpException('Seul admin@admin.com peut modifier les produits.');
+        }
+    }
+}
